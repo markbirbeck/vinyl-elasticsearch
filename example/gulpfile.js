78313ba9f29a6d4032882d595237b756d1ae3fb3
Mark Birbeck <mark.birbeck@gmail.com>
2015-04-28 22:34:34 +0100

    add example gulpfile

---
+++ example/gulpfile.js
@@ -0,0 +1,25 @@
+/**
+ * Run this from the root with:
+ *
+ *  gulp --cwd example/
+ */
+
+var gulp = require('gulp');
+
+var elasticsearch = require('../');
+
+gulp.task('default', function (){
+  return gulp.src('hello.json')
+    .pipe(
+      elasticsearch.dest(
+        {
+          index: 'someindex',
+          type: 'somedoc'
+        },
+        {
+          host: 'https://localhost:9200',
+          log: 'trace'
+        }
+      )
+    );
+});
