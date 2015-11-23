21c68702bd2362dc3781bd3e90bda1ecf0208eda
Mark Birbeck <mark.birbeck@gmail.com>
2015-11-23 17:03:24 +0000

    add to example gulpfile to illustrate use of src()

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -5,6 +5,7 @@
  */
 
 var gulp = require('gulp');
+var h = require('highland');
 
 var elasticsearch = require('../');
 
@@ -22,4 +23,45 @@ gulp.task('default', function() {
         }
       )
     );
+});
+
+function echo(file) {
+  return h()
+    .each(function(file) {
+      console.log('[ECHO]: ', file.id || file.path || 'no id or path',
+        file.data || String(file.contents));
+    });
+}
+
+gulp.task('search', function() {
+  return elasticsearch.src(
+    {
+      index: process.env.ELASTICSEARCH_INDEX,
+      size: 0,
+      body: {
+        // Begin query.
+        query: {
+          // Boolean query for matching and excluding items.
+          bool: {
+            must: [{match: {'organization': '/Organizaton/rayder'}}]
+          }
+        },
+        // Aggregate on the results
+        aggs: {
+          actions: {
+            terms: {
+              field: 'additionalType',
+              order: {'_term' : 'asc'},
+              size: 10,
+            }
+          }
+        }
+      }
+    },
+    {
+      host: process.env.ELASTICSEARCH_HOST,
+      log: 'trace'
+    }
+  )
+    .pipe(echo());
 });
